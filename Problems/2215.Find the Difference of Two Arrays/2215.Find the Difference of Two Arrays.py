da799c1cadc03f2b7b7194c359c6180c17b01644
Castella736 <42720537+Castella736@users.noreply.github.com>
2023-11-03 11:44:47 +0800

    Modify "2215.Find the Difference of Two Arrays.py": simplified syntax.

--- Problems/2215.Find the Difference of Two Arrays/2215.Find the Difference of Two Arrays.py
+++ Problems/2215.Find the Difference of Two Arrays/2215.Find the Difference of Two Arrays.py
@@ -1,5 +1,4 @@
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        table1 = set([nums1];
-        table2 = set([nums2];
-        return [list(table1-table2), list(table2-table1)];
+        table1, table2 = set(nums1), set(nums2);
+        return list(table1-table2), list(table2-table1);
