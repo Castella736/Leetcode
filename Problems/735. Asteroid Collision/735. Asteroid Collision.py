9826fbd77662a0364e39b5d23a7ec3be3630002a
Castella736 <42720537+Castella736@users.noreply.github.com>
2023-11-03 22:17:25 +0800

    Create "735. Asteroid Collision.py", "735. Asteroid Collision.md".

---
+++ Problems/735. Asteroid Collision/735. Asteroid Collision.py
@@ -0,0 +1,36 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        # Create a stack.
+        track = [];
+        # Check each asteroid
+        for newAsteroid in asteroids:
+            # Collision check until end.
+            while True:
+                if track and track[-1]>0 and newAsteroid<0:
+                    # The input always satisfies "left > 0 > right".
+                    # To check mass of which one is larger, is equivalent to compare "left" and "-right".
+                    # quivalently compare "left-(-right)" and "0".
+                    merge = track[-1] + newAsteroid;
+                    if merge==0:
+                        track.pop();
+                        break;
+                    elif merge>0:
+                        break;
+                    else:
+                        track.pop();
+                else:
+                    track.append(newAsteroid);
+                    break;
+                    
+        
+        return track;
+    
+    # This function returns the larger one.
+    def asteroidExplode(self, left: int, right: int) -> str:
+        merge = left+right;
+        if merge == 0:
+            return "both";
+        elif merge > 0:
+            return "left";
+        else:
+            return "right";
